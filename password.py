5ddcb0d507f43dc32bc4ce4f1a85e183b308f13c
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2019-04-17 14:36:08 -0300

    Reformatted using black.

--- password.py
+++ password.py
@@ -5,26 +5,30 @@ import time
 
 passwords = sys.argv[1:]
 if not passwords:
-    raise("Please supply at least one password")
+    raise ("Please supply at least one password")
 
 for password in passwords:
     sha1 = hashlib.sha1(password.encode()).hexdigest()
     to_send = sha1[:5]
-    url = f'https://api.pwnedpasswords.com/range/{to_send}'
-    
-    possibilities = requests.get(url).text.split('\r\n')
+    url = f"https://api.pwnedpasswords.com/range/{to_send}"
+
+    possibilities = requests.get(url).text.split("\r\n")
     for possibility in possibilities:
         if sha1[5:].upper() in possibility:
-            print('For password', password, 
-                    'there are', possibility.split(':')[1], 'leaks',
-                    flush=True)
+            print(
+                "For password",
+                password,
+                "there are",
+                possibility.split(":")[1],
+                "leaks",
+                flush=True,
+            )
             break
     else:
-        print(f'Your password, {password}, is safe! Nothing found',
-                flush=True)
-    
+        print(f"Your password, {password}, is safe! Nothing found", flush=True)
+
     sleep_time = 3
     for i in range(sleep_time, 0, -1):
-        print('\tSleeping for', i, 'seconds...\r', flush=True, end='')
+        print("\tSleeping for", i, "seconds...\r", flush=True, end="")
         time.sleep(1)
     print()
